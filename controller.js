f9e7df17bf2ab44fb388de5c51ff1a1c5c17b05d
JoseIvan <JoseIvan>
2016-12-21 23:43:45 -0800

    Avance1AppMD

---
+++ controller.js
@@ -0,0 +1,12 @@
+
+var myApp = angular.module('AppDesco',['ngMaterial', 'ngMessages'])
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      }
+    }
+  });
