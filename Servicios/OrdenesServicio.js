9e33d5ef89fc403baa869aa8a6adab07a43ee319
Diego Mondragon <mondragdiego@gmail.com>
2017-01-02 14:59:25 -0800

    Agregado factory ordenesServicio para comunicacion con el backend

---
+++ Servicios/OrdenesServicio.js
@@ -0,0 +1,46 @@
+myApp.factory('ordenesServicio', function(_env, $resource, $q){
+
+    var OrdenServicio = $resource(_env.urlApiDev+'/ordenesservicio/:id', {id: '@_id'}, {
+      update:{
+        method: 'PUT'
+      }
+    });
+
+    return {
+      agregar: agregar,
+      obtenerTodas: obtenerTodas,
+      obtenerPorId: obtenerPorId
+    }
+
+    function agregar(ordenServicio){
+      var deferred = $q.defer();
+      var nuevaOrdenServicio = new OrdenServicio();
+      angular.merge(nuevaOrdenServicio, ordenServicio);
+
+      OrdenServicio.save(nuevaOrdenServicio, function(respuesta){
+        deferred.resolve(respuesta);
+      });
+
+      return deferred.promise;
+    }
+
+    function obtenerTodas(){
+      var deferred = $q.defer();
+      var ordenesServicio = OrdenServicio.query(function(){
+          deferred.resolve(ordenesServicio);
+      });
+
+      return deferred.promise;
+    }
+
+    function obtenerPorId(idOrdenServicio){
+      var deferred = $q.defer();
+      var ordenServicio =  OrdenServicio.get({id: idOrdenServicio}, function(){
+        deferred.resolve(ordenServicio);
+      });
+
+      return deferred.promise;
+    }
+
+
+});
